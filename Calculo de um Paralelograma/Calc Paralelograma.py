1b9520b74a015b5b326f992e5af2842ef99b811a
Maria Eduarda Ciarini Moura <DudaCiarinii@gmail.com>
2023-07-13 21:10:23 -0300

    Comitando neste repositório
    Ajeitando os códigos e o Github

---
+++ Calculo de um Paralelograma/Calc Paralelograma.py
@@ -0,0 +1,8 @@
+# (Pseudocódigo) Calcular a Área de Um Paralelograma
+
+print("Bem-Vindo ao Calculador de Área de Paralelogramo")
+base = float(input("Comprimento da base: "))
+height = float(input("Insira a altura: "))
+area = base * height
+area
+print("A área do paralelogramo é: ", area)
